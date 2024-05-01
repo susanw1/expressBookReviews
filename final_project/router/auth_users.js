3a7d643df3ce2d95d83e77457985cf18b7e834b9
Susan <susanwitts1@gmail.com>
2024-05-01 11:31:04 -0400

    0. Add authentication

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,14 +9,19 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
+const authenticatedUser = (username, password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
 }
 
-//only registered users can login
+// only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const currUser = req.body.user;
+    if (!currUser) {
+        return res.status(404).json({message: "No data in body"});
+    }
+    const accessToken = jwt.sign({data: currUser}, 'access', {expiresIn: 3600 });
+    req.session.authorization = { accessToken };
+    return res.status(200).send(`User '${currUser.name}' logged in`);
 });
 
 // Add a book review
