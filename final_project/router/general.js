77ea5f2b4da45d84efbd641e1ced9fadae0cc0b8
susan <susanwitts1@gmail.com>
2024-05-02 11:25:04 -0400

    5. get book review

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -50,8 +50,17 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const matchingBooks = Object.entries(books)
+        .filter(([k, v]) => k == isbn)
+        .map(([k, v]) => v.reviews)
+        
+    if (matchingBooks.length == 0) {
+        return res.status(404).json( { message: `No book with ISBN '${isbn}'` } );
+    }
+    // should just be a single match, so assume [0]
+    const reviews = matchingBooks[0]
+    return res.status(200).send(JSON.stringify(reviews));
 });
 
 module.exports.general = public_users;
